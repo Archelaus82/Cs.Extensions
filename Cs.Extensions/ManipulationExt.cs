d7ff5dc00c7efbe11365b25b60620dfac3674559
Josh Holzworth <holzworthjosh@gmail.com>
2019-12-08 12:35:49 -0800

    changed namespace

--- Cs.Extensions/ManipulationExt.cs
+++ Cs.Extensions/ManipulationExt.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 
-namespace Cs.Extensions
+namespace Extensions
 {
     public class ManipulationExtensionException : Exception
     {
--- Cs.Extensions/ToStringExt.cs
+++ Cs.Extensions/ToStringExt.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cs.Extensions
+namespace Extensions
 {
     public static class ToStringExt
     {
